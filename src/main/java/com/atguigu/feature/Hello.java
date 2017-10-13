8e56a9800d9eb653957656a32e0eaab0ba543e9b
poplars <yangl@atguigu.com>
2017-10-13 11:41:51 +0800

    this is new project

---
+++ src/main/java/com/atguigu/feature/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu.feature;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("this is new project");
+		System.out.println("this is new project");
+		System.out.println("this is new project");
+		System.out.println("this is new project");
+	}
+}
