f48235e398178783f58877c647e278c264ad73a3
poplars <yangl@atguigu.com>
2017-10-13 11:49:28 +0800

    commit by east

--- src/main/java/com/atguigu/feature/Hello.java
+++ src/main/java/com/atguigu/feature/Hello.java
@@ -7,5 +7,11 @@ public class Hello {
 		System.out.println("this is new project");
 		System.out.println("this is new project");
 		System.out.println("this is new project");
+
+		System.out.println("this is create a new feature by east");
+		System.out.println("this is create a new feature by east");
+		System.out.println("this is create a new feature by east");
+		System.out.println("this is create a new feature by east");
+		System.out.println("this is create a new feature by east");
 	}
 }
